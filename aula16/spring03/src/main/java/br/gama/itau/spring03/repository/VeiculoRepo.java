e087dd03934a348d8f2903bf162c911d09b5d6d5
Flavio-GM <flavio_gm@yahoo.com.br>
2023-03-03 12:56:44 -0300

    aula16

---
+++ aula16/spring03/src/main/java/br/gama/itau/spring03/repository/VeiculoRepo.java
@@ -0,0 +1,9 @@
+package br.gama.itau.spring03.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import br.gama.itau.spring03.model.Veiculo;
+
+public interface VeiculoRepo extends CrudRepository<Veiculo, Long> {
+    
+}
